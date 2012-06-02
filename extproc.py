b06176ac3d62ed43fe0af40a2a16ab7ff964d54b
Paddy Mullen <paddy@paddymullen.com>
2012-06-02 11:00:44 -0400

    the big reindentation

--- extproc.py
+++ extproc.py
@@ -56,182 +56,186 @@ JOBS = []
 Capture = collections.namedtuple("Capture", "stdout stderr exit_status")
 
 def _is_fileno(n, f):
-  return (f is n) or (hasattr(f, 'fileno') and f.fileno() == n)
+    return (f is n) or (hasattr(f, 'fileno') and f.fileno() == n)
 
 def _name_or_self(f):
-  return (hasattr(f, 'name') and f.name) or f
+    return (hasattr(f, 'name') and f.name) or f
 
 
 class Cmd(object):
-  def __init__(self, cmd, fd={}, e={}, cd=None):
-    """
-    Prepare for a fork-exec of 'cmd' with information about changing
-    of working directory, extra environment variables and I/O
-    redirections if necessary.
+    def __init__(self, cmd, fd={}, e={}, cd=None):
+        """
+        Prepare for a fork-exec of 'cmd' with information about changing
+        of working directory, extra environment variables and I/O
+        redirections if necessary.
 
-    :param cmd: a list of command argurments.  If a string, it
-        is passed to shlex.split().
+        :param cmd: a list of command argurments.  If a string, it
+            is passed to shlex.split().
 
-    :param e: a dict of *extra* enviroment variables.
+        :param e: a dict of *extra* enviroment variables.
 
-    :param fd: a dict mapping k in [0, 1, 2] → v of type [file, string, int]
+        :param fd: a dict mapping k in [0, 1, 2] → v of type [file, string, int]
 
-      Whatever is pointed to by fd[0], fd[1] and fd[2] will become the
-      child's stdin, stdout and stderr, respectively.
+          Whatever is pointed to by fd[0], fd[1] and fd[2] will become the
+          child's stdin, stdout and stderr, respectively.
 
-      If any of key [0, 1, 2] is not specified, then it takes the
-      values [0, 1, 2] respectively -- in effect, reusing the parent's
-      [stdin, stdout, stderr].
+          If any of key [0, 1, 2] is not specified, then it takes the
+          values [0, 1, 2] respectively -- in effect, reusing the parent's
+          [stdin, stdout, stderr].
 
-      The value fd[k] can be of type
-      * file: always works and offer the most control over mode of operation
-      * string: works if can be open()'ed with mode 'r' when k == 0,
-        or mode 'w' for k in [1, 2]
-     * int: works for redirection {2: 1}
-            or {k: v} when v ≥ 3 and v is an existing file descriptor
+          The value fd[k] can be of type
+          * file: always works and offer the most control over mode of operation
+          * string: works if can be open()'ed with mode 'r' when k == 0,
+            or mode 'w' for k in [1, 2]
+         * int: works for redirection {2: 1}
+                or {k: v} when v ≥ 3 and v is an existing file descriptor
 
-    Note that the constructor only saves information in the object and
-    does not actually execute anything.
+        Note that the constructor only saves information in the object and
+        does not actually execute anything.
 
-    >>> Cmd("/bin/sh -c 'echo foo'")
-    Cmd(['/bin/sh', '-c', 'echo foo'], fd={0: 0, 1: 1, 2: 2}, e={}, cd=None)
+        >>> Cmd("/bin/sh -c 'echo foo'")
+        Cmd(['/bin/sh', '-c', 'echo foo'], fd={0: 0, 1: 1, 2: 2}, e={}, cd=None)
 
-    >>> Cmd(['grep', 'my stuff']) == Cmd('grep "my stuff"')
-    True
-    """
-    if isinstance(cmd, basestring):
-      self.cmd = shlex.split(cmd)
-    elif isinstance(cmd, (list, tuple)):
-      self.cmd = cmd
-    else:
-      raise TypeError("'cmd' must be either of type string, list or tuple")
-    self.cd = cd
-    self.e = e
-    self.env = os.environ.copy()
-    self.env.update(e)
-    self.fd = DEFAULT_FD.copy()
-    self.fd.update(fd)
+        >>> Cmd(['grep', 'my stuff']) == Cmd('grep "my stuff"')
+        True
+        """
+        if isinstance(cmd, basestring):
+            self.cmd = shlex.split(cmd)
+        elif isinstance(cmd, (list, tuple)):
+            self.cmd = cmd
+        else:
+            raise TypeError("'cmd' must be either of type string, list or tuple")
+        self.cd = cd
+        self.e = e
+        self.env = os.environ.copy()
+        self.env.update(e)
+        self.fd = DEFAULT_FD.copy()
+        self.fd.update(fd)
 
-    for stream_num in fd.keys():
-        if not isinstance(stream_num, int):
-            raise TypeError("fd keys must have type int")
-        elif stream_num < 0 or stream_num >= 3:
-            fd_num = fd[stream_num]
-            raise NotImplementedError(
-              "redirection {%s: %s} not supported" % (stream_num, fd_num))
-
-    for stream_num, fd_num in fd.iteritems():
-        if isinstance(fd_num, basestring):
-            self.fd[stream_num] = open(fd_num, 'r' if stream_num == 0 else 'w')
-        elif isinstance(fd_num, int):
-            if stream_num == 2 and fd_num == 1:
-                self.fd[STDERR] = _ORIG_STDOUT
-            elif (fd_num in (0, 1, 2)):
+        for stream_num in fd.keys():
+            if not isinstance(stream_num, int):
+                raise TypeError("fd keys must have type int")
+            elif stream_num < 0 or stream_num >= 3:
+                fd_num = fd[stream_num]
                 raise NotImplementedError(
                   "redirection {%s: %s} not supported" % (stream_num, fd_num))
-        elif stream_num is CLOSE:
-            raise NotImplementedError("closing is not supported")
-        elif not hasattr(fd_num, 'fileno'):
-            raise ValueError(
-              "fd value %s is not a file, string, int, or CLOSE" % (fd_num,))
 
-  def __repr__(self):
-    return "Cmd(%r, fd=%r, e=%r, cd=%r)" % (
-      self.cmd, dict((k, _name_or_self(v)) for k, v in self.fd.iteritems()),
-      self.e, self.cd)
+        for stream_num, fd_num in fd.iteritems():
+            if isinstance(fd_num, basestring):
+                self.fd[stream_num] = open(fd_num, 'r' if stream_num == 0 else 'w')
+            elif isinstance(fd_num, int):
+                if stream_num == 2 and fd_num == 1:
+                    self.fd[STDERR] = _ORIG_STDOUT
+                elif (fd_num in (0, 1, 2)):
+                    raise NotImplementedError(
+                      "redirection {%s: %s} not supported" % (stream_num, fd_num))
+            elif stream_num is CLOSE:
+                raise NotImplementedError("closing is not supported")
+            elif not hasattr(fd_num, 'fileno'):
+                raise ValueError(
+                  "fd value %s is not a file, string, int, or CLOSE" % (fd_num,))
 
-  def __eq__(self, other):
-    return (self.cmd == other.cmd) and (self.fd == other.fd) and\
-           (self.env == other.env) and (self.cd == other.cd)
+    def __repr__(self):
+      return "Cmd(%r, fd=%r, e=%r, cd=%r)" % (
+        self.cmd, dict((k, _name_or_self(v)) for k, v in self.fd.iteritems()),
+        self.e, self.cd)
 
-  def run(self):
-    """
-    Fork-exec the Cmd and waits for its termination.
+    def __eq__(self, other):
+      return (self.cmd == other.cmd) and (self.fd == other.fd) and\
+             (self.env == other.env) and (self.cd == other.cd)
 
-    Return the child's exit status.
+    def run(self):
+      """
+      Fork-exec the Cmd and waits for its termination.
 
-    >>> Cmd(['/bin/sh', '-c', 'exit 1']).run()
-    1
-    """
-    return subprocess.call(**self.popen_args)
+      Return the child's exit status.
 
-  def spawn(self):
-    """
-    Fork-exec the Cmd but do not wait for its termination.
+      >>> Cmd(['/bin/sh', '-c', 'exit 1']).run()
+      1
+      """
+      return subprocess.call(**self.popen_args)
 
-    Return a subprocess.Popen object (which is also stored in 'self.p')
-    """
-    self.p = self._popen()
-    JOBS.append(self)
-    return self.p
+    def spawn(self):
+      """
+      Fork-exec the Cmd but do not wait for its termination.
 
-  def capture(self, *fd):
-    """
-    Fork-exec the Cmd and wait for its termination, capturing the
-    output and/or error.
+      Return a subprocess.Popen object (which is also stored in 'self.p')
+      """
+      self.p = self._popen()
+      JOBS.append(self)
+      return self.p
 
-    :param fd: a list of file descriptors to capture,
-               should be a subset of [1, 2] where
-      * 1 represents the child's stdout
-      * 2 represents the child's stderr
+    def capture(self, *fd):
+        """
+        Fork-exec the Cmd and wait for its termination, capturing the
+        output and/or error.
 
-    Return a namedtuple (stdout, stderr, exit_status) where
-    stdout and stderr are captured file objects or None.
+        :param fd: a list of file descriptors to capture,
+                   should be a subset of [1, 2] where
+          * 1 represents the child's stdout
+          * 2 represents the child's stderr
 
-    Don't forget to close the file objects!
+        Return a namedtuple (stdout, stderr, exit_status) where
+        stdout and stderr are captured file objects or None.
 
-   >>> Cmd("/bin/sh -c 'echo -n foo'").capture(1).stdout.read()
-   'foo'
+        Don't forget to close the file objects!
 
-   >>> Cmd("/bin/sh -c 'echo -n bar >&2'").capture(2).stderr.read()
-   'bar'
+       >>> Cmd("/bin/sh -c 'echo -n foo'").capture(1).stdout.read()
+       'foo'
 
-   """
-    if not fd:
-      raise ValueError("what do you want to capture?")
-    if isinstance(fd, int):
-      fd = set([fd])
-    else:
-      fd = set(fd) or set([1])
-    if not fd <= set([1, 2]):
-      raise NotImplementedError(
-        "can only capture a subset of fd [1, 2] for now")
-    if STDOUT in fd:
-      if _is_fileno(STDOUT, self.fd[STDOUT]):
-        self.fd[STDOUT] = tempfile.TemporaryFile()
-      else:
-        raise ValueError(
-          "cannot capture the child's stdout: it had been redirected to %r"
-              % _name_or_self(self.fd[1]))
-    if 2 in fd:
-      if _is_fileno(2, self.fd[2]):
-        self.fd[2] = tempfile.TemporaryFile()
-      else:
-        raise ValueError(
-          "cannot capture the child's stderr: it had been redirected to %r"
-              % _name_or_self(self.fd[2]))
-    p = self._popen()
-    if p.stdin:
-      p.stdin.close()
-    p.wait()
-    if len(fd) == 1:
-      if 1 in fd:
-        if p.stderr: p.stderr.close()
-      else:
-        if p.stdout: p.stdout.close()
-    if 1 in fd: self.fd[1].seek(0)
-    if 2 in fd: self.fd[2].seek(0)
-    return Capture(self.fd[1], self.fd[2], p.returncode)
+       >>> Cmd("/bin/sh -c 'echo -n bar >&2'").capture(2).stderr.read()
+       'bar'
 
-  @property
-  def popen_args(self):
-    return dict(args=self.cmd, cwd=self.cd, env=self.env,
-    stdin=self.fd[0], stdout=self.fd[1], stderr=self.fd[2])
+       """
+        if not fd:
+            raise ValueError("what do you want to capture?")
+        if isinstance(fd, int):
+            fd = set([fd])
+        else:
+            fd = set(fd) or set([1])
+        if not fd <= set([1, 2]):
+            raise NotImplementedError(
+              "can only capture a subset of fd [1, 2] for now")
+        if STDOUT in fd:
+            if _is_fileno(STDOUT, self.fd[STDOUT]):
+                self.fd[STDOUT] = tempfile.TemporaryFile()
+            else:
+                raise ValueError(
+                  "cannot capture the child's stdout: it had been redirected to %r"
+                      % _name_or_self(self.fd[1]))
+        if 2 in fd:
+            if _is_fileno(2, self.fd[2]):
+                self.fd[2] = tempfile.TemporaryFile()
+            else:
+                raise ValueError(
+                  "cannot capture the child's stderr: it had been redirected to %r"
+                      % _name_or_self(self.fd[2]))
+        p = self._popen()
+        if p.stdin:
+            p.stdin.close()
+        p.wait()
+        if len(fd) == 1:
+            if 1 in fd:
+                if p.stderr:
+                    p.stderr.close()
+            else:
+                if p.stdout:
+                    p.stdout.close()
+        if 1 in fd:
+            self.fd[1].seek(0)
+        if 2 in fd:
+            self.fd[2].seek(0)
+        return Capture(self.fd[1], self.fd[2], p.returncode)
 
-  def _popen(self, **kwargs):
-      basic_popen_args = self.popen_args
-      basic_popen_args.update(kwargs)
-      return subprocess.Popen(**basic_popen_args)
+    @property
+    def popen_args(self):
+        return dict(args=self.cmd, cwd=self.cd, env=self.env,
+          stdin=self.fd[0], stdout=self.fd[1], stderr=self.fd[2])
+
+    def _popen(self, **kwargs):
+        basic_popen_args = self.popen_args
+        basic_popen_args.update(kwargs)
+        return subprocess.Popen(**basic_popen_args)
 
 class Sh(Cmd):
   def __init__(self, cmd, fd={}, e={}, cd=None):
@@ -260,11 +264,11 @@ class Pipe(object):
     self.env = os.environ.copy()
     self.env.update(self.e)
     for c in cmds:
-      c.e.update(self.e)
-      c.env.update(self.e)
+        c.e.update(self.e)
+        c.env.update(self.e)
     for c in cmds[:-1]:
-      if _is_fileno(1, c.fd[STDOUT]):
-        c.fd[STDOUT] = PIPE
+        if _is_fileno(1, c.fd[STDOUT]):
+          c.fd[STDOUT] = PIPE
     self.fd = {STDIN: cmds[0].fd[STDIN], STDOUT: cmds[-1].fd[STDOUT], 2: 2}
     self.cmds = cmds
 
@@ -279,13 +283,13 @@ class Pipe(object):
     """
     prev = self.cmds[0].fd[STDIN]
     for c in self.cmds:
-      c.p = c._popen(stdin=prev)
-      prev = c.p.stdout
+        c.p = c._popen(stdin=prev)
+        prev = c.p.stdout
     for c in self.cmds:
-      c.p.wait()
+        c.p.wait()
     for c in self.cmds[:-1]:
-      if c.fd[STDOUT] == PIPE:
-        c.p.stdout.close()
+        if c.fd[STDOUT] == PIPE:
+          c.p.stdout.close()
     return [c.p.returncode for c in self.cmds]
 
   def spawn(self):
@@ -299,8 +303,8 @@ class Pipe(object):
    """
     prev = self.cmds[0].fd[STDIN]
     for c in self.cmds:
-      c.p = c._popen(stdin=prev)
-      prev = c.p.stdout
+        c.p = c._popen(stdin=prev)
+        prev = c.p.stdout
     JOBS.append(self)
     return self
 
